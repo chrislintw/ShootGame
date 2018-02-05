8572276f7b627342e0e1a6b77eddcb2e21ab0d04
Otis Chou <altispec@gmail.com>
2018-02-06 01:13:59 +0800

    use webpack for development

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  devtool: 'inline-source-map'
+}
